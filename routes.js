52e09c0540502c1670a8d9db0d58811ae717ec2a
usamaajaved <usama-javed@hotmail.com>
2023-10-26 12:08:09 +0500

    updated routes

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const AuthRoutes = require("./routes/auth");
 const TaskRoutes = require("./routes/task");
 
 module.exports = function (app) {
+    //all routes
   app.use("/api/auth", AuthRoutes);
   app.use("/api/task", TaskRoutes);
 };
