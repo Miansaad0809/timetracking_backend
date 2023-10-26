7ab2c0c65c99e1b35f87428abceed5e8d2882fd5
Miansaad0809 <mian.saad0809@gmail.com>
2023-10-26 12:09:47 +0500

    updated routes

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ const AuthRoutes = require("./routes/auth");
 const TaskRoutes = require("./routes/task");
 
 module.exports = function (app) {
-    //all routes
   app.use("/api/auth", AuthRoutes);
   app.use("/api/task", TaskRoutes);
 };
